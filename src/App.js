648557aa7eedd7ac10498aab819b1c8c98bc8036
johnvees <yohanesvelly23@gmail.com>
2022-05-12 22:21:23 +0700

    add google oauth

--- src/App.js
+++ src/App.js
@@ -1,45 +1,46 @@
-import {StyleSheet, Text, View, TouchableOpacity, Alert} from 'react-native';
-import React, {useEffect, useState} from 'react';
-import messaging from '@react-native-firebase/messaging';
-import axios from 'axios';
-import {firebaseService} from './utils/api';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 
 const App = () => {
-  useEffect(() => {
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-    });
+  GoogleSignin.configure({
+    webClientId:
+      '973985605184-atf8ueaj79osgu9s2i3dr4j43brc6v4t.apps.googleusercontent.com',
+  });
 
-    return unsubscribe;
-  }, []);
+  const _signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(userInfo);
+    } catch (error) {
+      console.log(error);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
 
   return (
     <View>
-      <TouchableOpacity
-        onPress={async () => {
-          console.log(await messaging().getToken());
-        }}>
-        <Text>Remote Notification Foreground</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={async () => {
-          const body = {
-              to: 'coY4PZ5pQcyjABfYyaHcJS:APA91bGeodKUFs3zXGd7EnHMXWDBHfVnqUsLBfAjP0AnxvhDYmmaH5TmME-fkj4LH2PDkwIoHdDG7C71NhjnO4ogHSLKNMS09eFI4oshkJiz6koAprls1DN-AVruRUHlaSMJnLPG0pJ_',
-              notification: {
-                body: 'This is an FCM notification message!',
-                title: 'FCM Message',
-              },
-          };
-
-          const res = await axios.post(`${firebaseService}`, body, {
-            headers: {
-              Authorization: `Bearer AAAA4sYQ5kA:APA91bFicLBZqOTh-SpBtuNB6HZCBl2tE5HQnQySQGtmJLtnyGqCFgABJzZhvr-jXmMMiEiXe33zr0Ec6ahPJvjotPHkLNJNCjP_ZhNeBjXZUpsFsoiAFCeSi-Z217ExH2-j3qE1UKgO`,
-            },
-          });
-          console.log(res);
-        }}>
-        <Text>aaa</Text>
-      </TouchableOpacity>
+      <Text>App</Text>
+      <GoogleSigninButton
+        style={{width: 192, height: 48}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Light}
+        onPress={_signIn}
+        // disabled={this.state.isSigninInProgress}
+      />
     </View>
   );
 };
---
+++ src/screens/RemoteNotification.js
@@ -0,0 +1,49 @@
+import {StyleSheet, Text, View, TouchableOpacity, Alert} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import messaging from '@react-native-firebase/messaging';
+import axios from 'axios';
+import {firebaseService} from './utils/api';
+
+const RemoteNotification = () => {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <View>
+      <TouchableOpacity
+        onPress={async () => {
+          console.log(await messaging().getToken());
+        }}>
+        <Text>Remote Notification Foreground</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={async () => {
+          const body = {
+            to: 'coY4PZ5pQcyjABfYyaHcJS:APA91bGeodKUFs3zXGd7EnHMXWDBHfVnqUsLBfAjP0AnxvhDYmmaH5TmME-fkj4LH2PDkwIoHdDG7C71NhjnO4ogHSLKNMS09eFI4oshkJiz6koAprls1DN-AVruRUHlaSMJnLPG0pJ_',
+            notification: {
+              body: 'This is an FCM notification message!',
+              title: 'FCM Message',
+            },
+          };
+
+          const res = await axios.post(`${firebaseService}`, body, {
+            headers: {
+              Authorization: `Bearer AAAA4sYQ5kA:APA91bFicLBZqOTh-SpBtuNB6HZCBl2tE5HQnQySQGtmJLtnyGqCFgABJzZhvr-jXmMMiEiXe33zr0Ec6ahPJvjotPHkLNJNCjP_ZhNeBjXZUpsFsoiAFCeSi-Z217ExH2-j3qE1UKgO`,
+            },
+          });
+          console.log(res);
+        }}>
+        <Text>aaa</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default RemoteNotification;
+
+const styles = StyleSheet.create({});
