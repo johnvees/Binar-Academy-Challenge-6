7eaf0f34eeb476afa1402742cd061ae0fae64661
johnvees <yohanesvelly23@gmail.com>
2022-05-12 23:45:40 +0700

    add facebook oauth

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   GoogleSigninButton,
   statusCodes,
 } from '@react-native-google-signin/google-signin';
+import {AccessToken, LoginButton} from 'react-native-fbsdk-next';
 
 const App = () => {
   GoogleSignin.configure({
@@ -40,6 +41,20 @@ const App = () => {
         color={GoogleSigninButton.Color.Light}
         onPress={_signIn}
         // disabled={this.state.isSigninInProgress}
+      />
+      <LoginButton
+        onLoginFinished={(error, result) => {
+          if (error) {
+            console.log('login has error: ' + result.error);
+          } else if (result.isCancelled) {
+            console.log('login is cancelled.');
+          } else {
+            AccessToken.getCurrentAccessToken().then(data => {
+              console.log(data.accessToken.toString());
+            });
+          }
+        }}
+        onLogoutFinished={() => console.log('logout.')}
       />
     </View>
   );
