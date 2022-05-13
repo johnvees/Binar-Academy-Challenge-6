0e4618598b6bb99f76af8b40656d5bcdd4c586b0
johnvees <yohanesvelly23@gmail.com>
2022-05-13 16:57:27 +0700

    add log screen analytics

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,26 @@ import {NavigationContainer} from '@react-navigation/native';
 import MainStack from './MainStack';
 
 const Root = () => {
+  const routeNameRef = React.useRef();
+  const navigationRef = React.useRef();
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        routeNameRef.current = navigationRef.current.getCurrentRoute().name;
+      }}
+      onStateChange={async () => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.current.getCurrentRoute().name;
+
+        if (previousRouteName !== currentRouteName) {
+          await analytics().logScreenView({
+            screen_name: currentRouteName,
+            screen_class: currentRouteName,
+          });
+        }
+        routeNameRef.current = currentRouteName;
+      }}>
       <MainStack />
     </NavigationContainer>
   );
